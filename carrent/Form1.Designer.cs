0945c74dc03714cee778d6a0053144e04d4d15f8
irfan2019 <sam20will@gmail.com>
2022-06-05 15:17:01 +0530

    corrected

---
+++ carrent/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace carrent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nam = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dren = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cardrop = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(268, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "car rent";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // nam
+            // 
+            this.nam.Location = new System.Drawing.Point(25, 62);
+            this.nam.Name = "nam";
+            this.nam.Size = new System.Drawing.Size(205, 20);
+            this.nam.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "name";
+            // 
+            // dren
+            // 
+            this.dren.Location = new System.Drawing.Point(25, 135);
+            this.dren.Name = "dren";
+            this.dren.Size = new System.Drawing.Size(247, 20);
+            this.dren.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(512, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 67);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Submitting";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "date  rented";
+            // 
+            // cardrop
+            // 
+            this.cardrop.FormattingEnabled = true;
+            this.cardrop.Items.AddRange(new object[] {
+            "honda",
+            "toyo",
+            "hyun",
+            "volks"});
+            this.cardrop.Location = new System.Drawing.Point(25, 208);
+            this.cardrop.Name = "cardrop";
+            this.cardrop.Size = new System.Drawing.Size(121, 21);
+            this.cardrop.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 192);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "cars";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cardrop);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dren);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nam);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nam;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dren;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cardrop;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ carrent/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace carrent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String custname = nam.Text;
+                String date_returned = dren.Value.ToString();
+                var cartype = cardrop.SelectedItem.ToString();
+                var isInvalid = true;
+                var errormes = "";
+                if (string.IsNullOrWhiteSpace(custname) || string.IsNullOrWhiteSpace(cartype))
+                {
+                    isInvalid = false;
+                    MessageBox.Show("kindly enter the details ");
+                    errormes += "Please fill the correct message";
+                }
+                if (isInvalid)
+                {
+                    MessageBox.Show($"cusntomer name  {custname}\n" + $"date returnned  {date_returned}\n" + $"car type {cartype}\n" + "thank you for ordering");
+                }
+            }
+            catch(Exception ex)
+            {
+                
+                MessageBox.Show(ex.Message);
+            }
+
+                }
+        }
+    }
+
